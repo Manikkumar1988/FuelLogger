1e2e667b2f749ae20f32a228d8bac9e128a73933
Manikkumar <maniksrr@gmail.com>
2019-05-04 20:08:00 +0530

    enabled WORA tests

---
+++ app/src/sharedTest/java/com/mani/fuellog/MainActivityTest.java
@@ -0,0 +1,39 @@
+package com.mani.fuellog;
+
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertEquals;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Test
+    public void shouldHome() {
+        //ActivityScenario<MainActivity> j = ActivityScenario.launch(MainActivity.class);
+
+        /*j.moveToState(Lifecycle.State.CREATED);
+
+        j.onActivity(new ActivityScenario.ActivityAction<MainActivity>() {
+            @Override
+            public void perform(MainActivity activity) {
+                assertEquals(((TextView)activity.findViewById(R.id.message)).getText().toString(),"something");
+
+            }
+        });*/
+
+       // onView(withId(R.id.message)).perform(click());
+
+    }
+
+}
